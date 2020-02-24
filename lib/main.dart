d6910a98b4efc49a6d6d6cf06cc747e9683c73df
jesselima <jesselima.tech@gmail.com>
2020-02-24 16:45:00 -0300

    cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -9,20 +9,8 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blueGrey,
-        body: Center(
-          child: Image(
-            // Remote image sample
-            //image: NetworkImage('https://images.pexels.com/photos/459225/pexels-photo-459225.jpeg'),
-            // Asset image sample
-            image: AssetImage('images/hybrid.png'),
-          ),
-        ),
-        appBar: AppBar(
-            title: Text("My APp"),
-            backgroundColor:
-                Colors.blueGrey[900] // This set the AppBar background color.
-            ),
+        backgroundColor: Colors.teal,
+        body: Container(),
       ),
     ),
   );
