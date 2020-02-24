a3ddb28081ef376d336f1c14a761a5c0632d8516
jesselima <jesselima.tech@gmail.com>
2020-02-24 09:11:27 -0300

    project init

---
+++ android/app/src/main/kotlin/dev/jesselima/flutter_bootcamp/MainActivity.kt
@@ -0,0 +1,12 @@
+package dev.jesselima.flutter_bootcamp
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
